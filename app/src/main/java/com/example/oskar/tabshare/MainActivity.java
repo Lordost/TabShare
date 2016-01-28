1351fcd76ee1941db60447e7da643b5149cd7cb0
Lordost <LordostHH@gmail.com>
2016-01-28 16:12:19 +0100

    Inital commit

---
+++ app/src/main/java/com/example/oskar/tabshare/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.oskar.tabshare;
+
+import android.content.ClipboardManager;
+import android.os.StrictMode;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class MainActivity extends AppCompatActivity {
+Button _Button;
+EditText _EditText, _EditText2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        
+        _Button = (Button)this.findViewById(R.id.button);
+        _EditText = (EditText)this.findViewById(R.id.editText);
+        _EditText2 = (EditText)this.findViewById(R.id.editText2);
+        
+        _Button.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+            String ip;
+                ip = _EditText.getText().toString();
+                String url = _EditText2.getText().toString();
+                try {
+                    SendInformationToServer(ip, 6000, url);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            
+            
+            
+        });
+    }
+    public static void SendInformationToServer(String ip, int port, String data) throws Exception {
+        Socket clientSocket = new Socket(ip, port);
+        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+        outToServer.writeBytes(data + '\n');
+        clientSocket.close();
+    }
+}
