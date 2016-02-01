247222a3dcd3e235a94be6aac91c4f219d3225c4
Lordost <LordostHH@gmail.com>
2016-02-01 10:44:49 +0100

    Added Find Server button changed connection type

--- app/src/main/java/com/example/oskar/tabshare/MainActivity.java
+++ app/src/main/java/com/example/oskar/tabshare/MainActivity.java
@@ -1,52 +1,66 @@
 package com.example.oskar.tabshare;
 
-import android.content.ClipboardManager;
+import android.os.Bundle;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import java.io.DataOutputStream;
-import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
+import java.util.ArrayList;
+
+import net.daedricwolf.clientTools.client;
 
 public class MainActivity extends AppCompatActivity {
 Button _Button;
+Button _Button2;
 EditText _EditText2;
+ArrayList <InetAddress> _Servers = new ArrayList<InetAddress>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         
         _Button = (Button)this.findViewById(R.id.button);
+        _Button2 = (Button)this.findViewById(R.id.button2);
         _EditText2 = (EditText)this.findViewById(R.id.editText2);
-        
-        _Button.setOnClickListener(new Button.OnClickListener(){
+
+        _Button2.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View v) {
-            String ip;
-                String url = _EditText2.getText().toString();
                 try {
-                    SendInformationToServer("192.168.0.23", 6000, url);
+                    client.FindServers(_Servers ,6000);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
-            
-            
-            
+
+
+        });
+
+        _Button.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ip;
+                String url = _EditText2.getText().toString();
+
+                try {
+                    client.SendInformationToServer(_Servers, 0, 6000, url);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+
         });
     }
-    public static void SendInformationToServer(String ip, int port, String data) throws Exception {
-        Socket clientSocket = new Socket(ip, port);
-        DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-        outToServer.writeBytes(data + '\n');
-        clientSocket.close();
-    }
+
 }
